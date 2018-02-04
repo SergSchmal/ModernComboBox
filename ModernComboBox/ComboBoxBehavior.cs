3fa200a765a3562312ad660fcb7abf3caa3ec770
Sergej Schmal <serg.schmal@gmx.de>
2018-02-04 08:52:22 +0100

    Add project files.

---
+++ ModernComboBox/ComboBoxBehavior.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+using System.Windows.Media;
+
+namespace ModernComboBox
+{
+    public class ComboBoxBehavior : Behavior<ComboBox>
+    {
+        protected override void OnAttached()
+        {
+            AssociatedObject.DropDownClosed += AssociatedObjectOnDropDownClosed;
+            AssociatedObject.SelectionChanged += AssociatedObject_SelectionChanged;
+        }
+
+        private void AssociatedObject_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var combo = sender as ComboBox;
+
+            if (combo != null)
+            {
+                combo.IsDropDownOpen = true;
+            }
+        }
+
+        private void AssociatedObjectOnDropDownClosed(object sender, EventArgs eventArgs)
+        {
+            var parent = VisualTreeHelper.GetParent((DependencyObject) sender);
+            Point m = Mouse.GetPosition((IInputElement) parent);
+            VisualTreeHelper.HitTest((Visual) parent, FilterCallback, ResultCallback, new PointHitTestParameters(m));
+        }
+
+        private HitTestFilterBehavior FilterCallback(DependencyObject o)
+        {
+            var c = o as Control;
+            if (o is MainWindow || c == null || !c.Focusable) return HitTestFilterBehavior.Continue;
+            if (c is ComboBox box && !box.IsDropDownOpen)
+            {
+                // This go exception
+              //box.IsDropDownOpen = true;
+            }
+            else
+            {
+                var mouseDevice = Mouse.PrimaryDevice;
+                var mouseButtonEventArgs = new MouseButtonEventArgs(mouseDevice, 0, MouseButton.Left)
+                {
+                    RoutedEvent = Mouse.MouseDownEvent,
+                    Source = c
+                };
+                c.RaiseEvent(mouseButtonEventArgs);
+            }
+
+            return HitTestFilterBehavior.Stop;
+        }
+
+        private HitTestResultBehavior ResultCallback(HitTestResult r)
+        {
+            return HitTestResultBehavior.Continue;
+        }
+    }
+}
---
+++ ModernComboBox/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ModernComboBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var button = (Button) sender;
+            MessageBox.Show("Was clicked " + button.Content);
+        }
+    }
+}
